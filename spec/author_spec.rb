30bd431f0ce55fc55eb15bffd8ccdcf1c4eef075
github username <github email address>
2019-10-14 14:07:58 -0700

    Done.

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "pry"
 
 describe "Author" do
 
@@ -37,7 +38,6 @@ describe "Author" do
     describe "#add_post_by_title" do
       it "takes in an argument of a post title, creates a new post with it and associates the post and author" do
         betty.add_post_by_title("My Great Blog Post")
-        # binding.pry
         expect(betty.posts.last.title).to eq("My Great Blog Post")
         expect(betty.posts.last.author).to eq(betty)
       end
@@ -46,7 +46,7 @@ describe "Author" do
     describe ".post_count" do
       it "is a class method that returns the total number of posts associated to all existing authors" do
         expect(Author.post_count).to eq(3)
+        
       end
     end
-
 end
